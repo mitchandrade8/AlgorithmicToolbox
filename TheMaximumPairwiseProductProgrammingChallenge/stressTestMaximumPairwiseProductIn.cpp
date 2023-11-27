69a9dc11a40b415eaf5b86e407dc84e936fe18c5
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-27 15:00:43 -0700

    --Learning how to stress test an algorithm in Cpp

---
+++ TheMaximumPairwiseProductProgrammingChallenge/stressTestMaximumPairwiseProductIn.cpp
@@ -0,0 +1,19 @@
+
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+
+using std::vector;
+using std::cin;
+using std::cout;
+
+long long MaxPairwiseProduct(const vector<int>& numbers) {
+    long long result = 0;
+    int n = numbers.size();
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; ++j) {
+            
+        }
+    }
+}
