c3883b3835fd0ff3b6e50c3421bca0ae5c21c59e
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-27 06:41:55 -0700

    --Completed SumOfTwoDigitsIn Cpp

--- SolvingTheSumofTwoDigitsProgrammingChallenge/SumOfTwoDigitsIn.cpp
+++ SolvingTheSumofTwoDigitsProgrammingChallenge/SumOfTwoDigitsIn.cpp
@@ -1,1 +1,17 @@
 
+#include <iostream>
+
+int main() {
+    int a = 0;
+    int b = 0;
+    int sum = 0;
+    std::cout << "Please enter value for a: ";
+    std::cin >> a;
+    std::cout << "Please enter value for b: ";
+    std::cin >> b;
+
+    sum = a + b;
+    std::cout << sum;
+
+    return 0;
+}
